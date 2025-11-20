ecc389ef1103a8725750782cc510168fab085224
Sunil Salaria <sunil.salaria@lnpinfotech.com>
2025-11-20 16:24:26 +0530

    basic setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+       <div className="p-10 bg-purple-600 text-white text-3xl">
+      Tailwind is working! 🎉
+    </div>
+    </>
+  )
+}
+
+export default App
